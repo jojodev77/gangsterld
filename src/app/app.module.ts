1676c4a1012aded7952c1bad40b69bd5a4644bc0
jojodev77 <jonathandelaosa@gmail.com>
2020-06-20 00:42:55 +0200

    update ang 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +35,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatIconModule,
     MatCardModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
   ],
   providers: [GestionService],
   bootstrap: [AppComponent]
